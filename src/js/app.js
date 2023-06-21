4d869b4eec0f94e27fb5aed465f9e28d67c0bc48
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-06-21 20:10:24 +0000

    Variables render

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { left, right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -33,10 +34,10 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name ? variables.name: 'Name'}, ${variables.lastname ? variables.lastname: 'Last Name'}</h1>
+          <h2>${variables.role ? variables.role: 'Rol'}</h2>
+          <h3>${variables.country ? variables.country: 'Country'}, ${variables.city ? variables.city: 'City'}</h3>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
